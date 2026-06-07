962f571b7e763d0d5062b354c40fb76aadeb60df
Ayush Malviya <ayushmalviya078@gmail.com>
2026-06-07 14:07:49 +0530

    Fix chat WebSocket payload handling and add .gitignore

--- justchat/justchat/settings.py
+++ justchat/justchat/settings.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-
+import os 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -110,6 +110,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 CHANNEL_LAYERS = {
     "default": {
         "BACKEND": "channels_redis.core.RedisChannelLayer",
